24ffb1635a19dfe6384532109f98b75bb9319c6c
Thomas Desmond <24610108+thomas-desmond@users.noreply.github.com>
2022-10-17 07:53:45 -0700

    Modifying server side auth

--- pages/server-auth.tsx
+++ pages/server-auth.tsx
@@ -1,30 +1,25 @@
 import { unstable_getServerSession } from "next-auth/next"
 import { authOptions } from "./api/auth/[...nextauth]"
-import { getSession, useSession } from "next-auth/react"
-import { Session, SessionOptions } from "next-auth"
 
 export async function getServerSideProps(context: any) {
-    const session = await unstable_getServerSession(context.req, context.res, authOptions)
+    const session = await unstable_getServerSession(
+        context.req, context.res, authOptions)
 
-    if (!session) {
+    if (session) {
         return {
-            redirect: { destination: "/login", permanent: false }
+            props: { username: session?.user?.name }
         }
     }
     return {
-        props: { auth: session.user?.name }
+        redirect: { destination: "/", permanent: false }
     }
-
 }
 
-export default function ServerSideAuth(session: Session) {
-    if (session) {
-        return (
-            <>
-                <h1>Protected Page</h1>
-                <p>You can view this page because you are signed in.</p>
-            </>
-        )
-    }
-    return <p>Access Denied - should have redirected</p>
+export default function ServerSideAuth(username: string) {
+    return (
+        <>
+            <h1>Protected Page</h1>
+            <p>You can view this page because you are signed in as {username}</p>
+        </>
+    )
 }
